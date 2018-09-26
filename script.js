e086bc652bf58feca44af66cbd2ea4543c0eb7da
Sam Kauffman <samvnkauffman@gmail.com>
2018-09-26 13:43:17 -0400

    ignore 'action-menu-item' dropdowns

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function fixPhpNetTitles() {
-    const phpNetAnchors = document.querySelectorAll('[href*="php.net/manual/"]');
+    const phpNetAnchors = document.querySelectorAll(':not(li) > [href*="php.net/manual/"]'); // ignore 'action-menu-item' dropdowns
 
     phpNetAnchors.forEach((node) => {
         let functionName = (/function\.(.+)\.php/).exec(node.href);
